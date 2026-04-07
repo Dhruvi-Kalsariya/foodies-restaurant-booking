4ca5453a10c1f63992cf3b244cb10086968f668a
dhruvi9699 <dhruvikalsariya70@gmail.com>
2026-04-07 14:17:17 +0530

    Initial commit - FOODIES project

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">FOODIES</div>
+          <div className="right">
+            <p>D6 Kotecha Chock, Rajkot, Gujarat, India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className='banner'>
+          <div className='left'>
+            <p>Developed By Dhruvi-Kalsariya</p>
+          </div>
+          <div className="right">
+            <p>All rights Reserved by Dhruvi-Kalsariya</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
